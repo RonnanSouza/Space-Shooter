fb6e04fa1e9e65c63cd45a047fc3704a151ae78d
ronnansouza <ronan.souza@ccc.ufcg.edu.br>
2019-03-16 16:00:31 -0300

    Spawning waves

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject hazard;
-    public Vector3 SpawnValues;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float startWait;
+    public float waitSpawn;
+    public float waveWait;
 
     void Start()
     {
-        SpawnWaves();
+
+        StartCoroutine(SpawnWaves());
     }
 
-    void SpawnWaves()
+    IEnumerator SpawnWaves()
     {
-        Vector3 SpawnPosition = new Vector3(Random.Range(-SpawnValues.x, SpawnValues.x), SpawnValues.y, SpawnValues.z);
-        Quaternion SpawnRotation = Quaternion.identity;
-        Instantiate(hazard, SpawnPosition, SpawnRotation);
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 SpawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion SpawnRotation = Quaternion.identity;
+                Instantiate(hazard, SpawnPosition, SpawnRotation);
+                yield return new WaitForSeconds(waitSpawn);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+        
+        
     }
 }
