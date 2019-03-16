2309bdbec2130ccae28b836134c452ee8fe2de5f
ronnansouza <ronan.souza@ccc.ufcg.edu.br>
2019-03-16 14:31:23 -0300

    Creating hazards

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+
+}
