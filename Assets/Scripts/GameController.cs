7a849a1d92f3c41d4418ad0845eeee3570f6803a
ronnansouza <ronan.souza@ccc.ufcg.edu.br>
2019-03-16 15:19:27 -0300

    Game Controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 SpawnValues;
+
+    void Start()
+    {
+        SpawnWaves();
+    }
+
+    void SpawnWaves()
+    {
+        Vector3 SpawnPosition = new Vector3(Random.Range(-SpawnValues.x, SpawnValues.x), SpawnValues.y, SpawnValues.z);
+        Quaternion SpawnRotation = Quaternion.identity;
+        Instantiate(hazard, SpawnPosition, SpawnRotation);
+    }
+}
